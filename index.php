1b471243bd80e1a3cb13403a5187bf903789bc27
kupshit <kod.tmd@gmail.com>
2018-10-15 14:41:45 +0700

    Update index html

--- index.php
+++ index.php
@@ -1,1 +1,205 @@
-﻿<h1> Hello Word</h1>
+<!DOCTYPE html><html lang="vi"><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> Dashboard | Manager file </title>
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://uselooper.com/assets/apple-touch-icon.png">
+    <link rel="shortcut icon" href="http://uselooper.com/assets/favicon.ico">
+    <meta name="theme-color" content="#3063A0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link href="main.min.css" rel="stylesheet">
+</head>
+  <body>
+    <div class="app">
+      <main>
+        <div class="wrapper">
+          <div class="page">
+            <div class="page-inner">
+              <div class="page-section">
+                <div class="section-deck">
+                  <!-- .card -->
+                  <div class="card card-fluid">
+                    <header class="card-header"> Active Projects </header>
+                    <!-- .lits-group -->
+                    <div class="lits-group list-group-flush">
+                      <!-- .lits-group-item -->
+                      <div class="list-group-item">
+                        <!-- .lits-group-item-figure -->
+                        <div class="list-group-item-figure">
+                          <div class="has-badge">
+                            <a href="app-tasks.html" class="tile tile-md bg-blue">LT</a>
+                            <a href="#team" class="user-avatar user-avatar-xs">
+                              <img src="assets/images/avatars/team1.jpg" alt="">
+                            </a>
+                          </div>
+                        </div>
+                        <!-- .lits-group-item-figure -->
+                        <!-- .lits-group-item-body -->
+                        <div class="list-group-item-body">
+                          <h5 class="card-title mb-2">
+                            <a href="app-tasks.html">Looper Admin Theme</a>
+                          </h5>
+                          <p class="card-subtitle text-muted"> Progress in 74% - Last update 1d </p>
+                          <!-- .progress -->
+                          <div class="progress progress-xs bg-white">
+                            <div class="progress-bar bg-blue" role="progressbar" aria-valuenow="2181" aria-valuemin="0" aria-valuemax="100" style="width: 74%">
+                              <span class="sr-only">74% Complete</span>
+                            </div>
+                          </div>
+                          <!-- /.progress -->
+                        </div>
+                        <!-- .lits-group-item-body -->
+                      </div>
+                      <!-- /.lits-group-item -->
+                      <!-- .lits-group-item -->
+                      <div class="list-group-item">
+                        <!-- .lits-group-item-figure -->
+                        <div class="list-group-item-figure">
+                          <div class="has-badge">
+                            <a href="app-tasks.html" class="tile tile-md bg-indigo">SP</a>
+                            <a href="#team" class="user-avatar user-avatar-xs">
+                              <img src="assets/images/avatars/team1.jpg" alt="">
+                            </a>
+                          </div>
+                        </div>
+                        <!-- .lits-group-item-figure -->
+                        <!-- .lits-group-item-body -->
+                        <div class="list-group-item-body">
+                          <h5 class="card-title mb-2">
+                            <a href="app-tasks.html">Smart Paper</a>
+                          </h5>
+                          <p class="card-subtitle text-muted"> Progress in 22% - Last update 2h </p>
+                          <!-- .progress -->
+                          <div class="progress progress-xs bg-white">
+                            <div class="progress-bar bg-indigo" role="progressbar" aria-valuenow="867" aria-valuemin="0" aria-valuemax="100" style="width: 22%">
+                              <span class="sr-only">22% Complete</span>
+                            </div>
+                          </div>
+                          <!-- /.progress -->
+                        </div>
+                        <!-- .lits-group-item-body -->
+                      </div>
+                      <!-- /.lits-group-item -->
+                      <!-- .lits-group-item -->
+                      <div class="list-group-item">
+                        <!-- .lits-group-item-figure -->
+                        <div class="list-group-item-figure">
+                          <div class="has-badge">
+                            <a href="app-tasks.html" class="tile tile-md bg-yellow">OS</a>
+                            <a href="#team" class="user-avatar user-avatar-xs">
+                              <img src="assets/images/avatars/team2.png" alt="">
+                            </a>
+                          </div>
+                        </div>
+                        <!-- .lits-group-item-figure -->
+                        <!-- .lits-group-item-body -->
+                        <div class="list-group-item-body">
+                          <h5 class="card-title mb-2">
+                            <a href="app-tasks.html">Online Store</a>
+                          </h5>
+                          <p class="card-subtitle text-muted"> Progress in 99% - Last update 2d </p>
+                          <!-- .progress -->
+                          <div class="progress progress-xs bg-white">
+                            <div class="progress-bar bg-yellow" role="progressbar" aria-valuenow="6683" aria-valuemin="0" aria-valuemax="100" style="width: 99%">
+                              <span class="sr-only">99% Complete</span>
+                            </div>
+                          </div>
+                          <!-- /.progress -->
+                        </div>
+                        <!-- .lits-group-item-body -->
+                      </div>
+                      <!-- /.lits-group-item -->
+                      <!-- .lits-group-item -->
+                      <div class="list-group-item">
+                        <!-- .lits-group-item-figure -->
+                        <div class="list-group-item-figure">
+                          <div class="has-badge">
+                            <a href="app-tasks.html" class="tile tile-md bg-purple">BA</a>
+                            <a href="#team" class="user-avatar user-avatar-xs">
+                              <img src="assets/images/avatars/team3.png" alt="">
+                            </a>
+                          </div>
+                        </div>
+                        <!-- .lits-group-item-figure -->
+                        <!-- .lits-group-item-body -->
+                        <div class="list-group-item-body">
+                          <h5 class="card-title mb-2">
+                            <a href="app-tasks.html">Booking App</a>
+                          </h5>
+                          <p class="card-subtitle text-muted"> Progress in 35% - Last update 4h </p>
+                          <!-- .progress -->
+                          <div class="progress progress-xs bg-white">
+                            <div class="progress-bar bg-purple" role="progressbar" aria-valuenow="112" aria-valuemin="0" aria-valuemax="100" style="width: 35%">
+                              <span class="sr-only">35% Complete</span>
+                            </div>
+                          </div>
+                          <!-- /.progress -->
+                        </div>
+                        <!-- .lits-group-item-body -->
+                      </div>
+                      <!-- /.lits-group-item -->
+                      <!-- .lits-group-item -->
+                      <div class="list-group-item">
+                        <!-- .lits-group-item-figure -->
+                        <div class="list-group-item-figure">
+                          <div class="has-badge">
+                            <a href="app-tasks.html" class="tile tile-md bg-teal">SB</a>
+                            <a href="#team" class="user-avatar user-avatar-xs">
+                              <img src="assets/images/avatars/team3.png" alt="">
+                            </a>
+                          </div>
+                        </div>
+                        <!-- .lits-group-item-figure -->
+                        <!-- .lits-group-item-body -->
+                        <div class="list-group-item-body">
+                          <h5 class="card-title mb-2">
+                            <a href="app-tasks.html">SVG Icon Bundle</a>
+                          </h5>
+                          <p class="card-subtitle text-muted"> Progress in 32% - Last update 1d </p>
+                          <!-- .progress -->
+                          <div class="progress progress-xs bg-white">
+                            <div class="progress-bar bg-teal" role="progressbar" aria-valuenow="461" aria-valuemin="0" aria-valuemax="100" style="width: 32%">
+                              <span class="sr-only">32% Complete</span>
+                            </div>
+                          </div>
+                          <!-- /.progress -->
+                        </div>
+                        <!-- .lits-group-item-body -->
+                      </div>
+                      <!-- /.lits-group-item -->
+                      <!-- .lits-group-item -->
+                      <div class="list-group-item">
+                        <!-- .lits-group-item-figure -->
+                        <div class="list-group-item-figure">
+                          <div class="has-badge">
+                            <a href="app-tasks.html" class="tile tile-md bg-pink">SP</a>
+                            <a href="#team" class="user-avatar user-avatar-xs">
+                              <img src="assets/images/avatars/team1.jpg" alt="">
+                            </a>
+                          </div>
+                        </div>
+                        <!-- .lits-group-item-figure -->
+                        <!-- .lits-group-item-body -->
+                        <div class="list-group-item-body">
+                          <h5 class="card-title mb-2">
+                            <a href="app-tasks.html">Syrena Project</a>
+                          </h5>
+                          <p class="card-subtitle text-muted"> Progress in 93% - Last update 8h </p>
+                          <!-- .progress -->
+                          <div class="progress progress-xs bg-white">
+                            <div class="progress-bar bg-pink" role="progressbar" aria-valuenow="3981" aria-valuemin="0" aria-valuemax="100" style="width: 93%">
+                              <span class="sr-only">93% Complete</span>
+                            </div>
+                          </div>
+                          <!-- /.progress -->
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+</body></html>
